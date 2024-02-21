60f68cfa3e5350cda769b60c48142e7f0717b750
evatc <eva.torresc@edu.uah.es>
2024-02-21 14:07:00 +0100

    test

---
+++ test/java/es/uah/matcomp/mp/e1/ejerciciosclases/ej1/Circle1Test.java
@@ -0,0 +1,32 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.ej1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Circle1Test {
+
+    @Test
+    void getRadius() {
+    }
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void setRadius() {
+    }
+
+    @Test
+    void setColor() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
