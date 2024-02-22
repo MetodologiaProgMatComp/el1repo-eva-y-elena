c48f10f57895cc661ea32dc315bf29e8f76616fe
elenagarcia05 <elena.garcialorente@edu.uah.es>
2024-02-22 22:26:36 +0100

    ej 1 y 2(no esta del todo bien)

---
+++ src/es/uah/matcomp/mp/e2/ejerciciosclases/ej2/Book.java
@@ -0,0 +1,54 @@
+package es.uah.matcomp.mp.e2.ejerciciosclases.ej2;
+
+public class Book {
+    private String name;
+    private Author[] authors;
+    private double price;
+    private int qty;
+    public Book(String name, Author[] authors, double price){
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+        qty = 0;
+    }
+    public Book(String name, Author[] authors, double price, int qty){
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+    public String toString() {
+
+        return "Book[name=" + name + "," + authors + ",price=" + price + ",qty=" + qty + "]";
+    }
+    public String getAuthorNames() {
+        return name;
+    }
+
+}
+
+
---
+++ src/es/uah/matcomp/mp/e2/ejerciciosclases/ej5/Customer.java
@@ -0,0 +1,4 @@
+package es.uah.matcomp.mp.e2.ejerciciosclases.ej5;
+
+public class Customer {
+}
