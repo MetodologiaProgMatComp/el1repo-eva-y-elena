d93b3c8e9dd20059f22001f0b1e9185d5b063ad9
elenagarcia05 <elena.garcialorente@edu.uah.es>
2024-02-28 13:09:01 +0100

    ej 3

---
+++ src/es/uah/matcomp/mp/e3/ejerciciosclases/ej3/Point3D.java
@@ -0,0 +1,41 @@
+package es.uah.matcomp.mp.e3.ejerciciosclases.ej3;
+
+public class Point3D extends Point2D{
+    private float z;
+
+    public Point3D(float x, float y, float z) {
+        setX(x);
+        setY(y);
+        this.z = z;
+    }
+
+    public Point3D() {
+        this.z = 0.0f;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    public void setXYZ(float x, float y, float z) {
+        setX(x);
+        setY(y);
+        this.z = z;
+    }
+    public float[] getXYZ() {
+        float[] result = new float[3]; // construct an array of 3 elements
+        result[0] = getX();
+        result[1] = getY();
+        result[2] = z;
+        return result; // return the array
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getX() + "," + getY() + "," + z + ")";
+
+    }
+}
